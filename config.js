5d7086e6fbbec14248372c9075c4da4c721d2c93
GADI TEJA SRI LAKSHMI SAI RAM <teja050504@gmail.com>
2025-07-27 18:27:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
 // Replace with your actual OpenRouter API key
-``
 export const OPENROUTER_API_KEY = process.env.OPENROUTER_API_KEY || "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";
